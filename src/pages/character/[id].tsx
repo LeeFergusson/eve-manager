764bd53184c6c530fe9bc724a96a903a7743c649
Lee Fergusson <lee.fergusson@googlemail.com>
2023-01-30 09:12:07 +0000

    Add CharacterDetails to character/[id] page

--- src/pages/character/[id].tsx
+++ src/pages/character/[id].tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import CharacterDetails from "../../components/ui/CharacterDetails";
 
 const CharacterPage: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  return <section>{id}</section>;
+  return (
+    <article>
+      <h2>Character Details</h2>
+      <div className="grid grid-cols-2 gap-2">
+        <CharacterDetails name="Test" />
+        <p>{id}</p>
+      </div>
+    </article>
+  );
 };
 
 export default CharacterPage;
