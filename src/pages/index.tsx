1619b137e96a53820d89b49af701ee2dcce20781
Lee Fergusson <lee.fergusson@googlemail.com>
2023-02-03 13:34:05 +0000

    Move HeroSection to components/ui folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { signIn } from "next-auth/react";
+
+import HeroSection from "../components/ui/HeroSection";
+import NewsFeed from "../components/NewsFeed";
 
 const Home: NextPage = () => {
   return (
@@ -8,12 +12,47 @@ const Home: NextPage = () => {
         <title>Home Base</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col p-2">
-        <h2 className="text-xl">Home</h2>
-        <p>Welcome to the application!</p>
+      <main className="flex flex-col gap-2 p-4">
+        <article className="mx-auto">
+          <h2 className="hidden">Home</h2>
+
+          <div className="flex gap-2">
+            <HeroSection>
+              <h3 className="text-xl font-semibold">
+                Welcome to the application!
+              </h3>
+              <p className="max-w-[35em] p-2 text-lg">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+                voluptas nulla incidunt, laudantium ullam aperiam iure quasi ea
+                quas dignissimos praesentium, consectetur, accusantium
+                cupiditate explicabo. Voluptates veniam quasi possimus
+                distinctio!
+              </p>
+              <button
+                className="m-1 rounded-md bg-gray-400 p-2 shadow-md"
+                onClick={() => void signIn()}
+              >
+                Sign-In
+              </button>
+            </HeroSection>
+            <NewsFeed />
+          </div>
+        </article>
       </main>
     </>
   );
 };
+
+// interface HeroSectionProps {
+//   children?: ReactElement | ReactElement[] | string;
+// }
+
+// const HeroSection = (props: HeroSectionProps) => {
+//   return (
+//     <section className="flex-shrink rounded-sm border-l-4 border-blue-500 bg-white p-6 shadow-md">
+//       {props.children}
+//     </section>
+//   );
+// };
 
 export default Home;
